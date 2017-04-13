e8b46dad5df63fb5b2a59758ec7d87dc3f5b5b8e
igusa@mail.ru <gtx386i>
2017-04-13 15:58:04 +0300

    3

---
+++ src/pkg/firstJavaSnake/HelloComponent3.java
@@ -0,0 +1,148 @@
+package pkg.firstJavaSnake;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Random;
+
+class HelloComponent3 extends JComponent implements Runnable // implements MouseMotionListener
+{
+
+    int gridCountX=10,gridCountY=10;
+    int stepX;
+    int stepY;
+
+    Thread myThread;
+
+    ArrayList<Snake> snakeList;
+    ArrayList<Point> foodList;
+
+    private void Sleep (long millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {}
+
+    }
+
+
+
+    public void run() {
+
+        Sleep(1000);
+        while(true) {
+
+            for (int i=0 ; i<snakeList.size();i++) {
+                Date c0 = new Date();
+                Snake s = snakeList.get(i);
+                PathFind pathFind = new PathFind(snakeList,foodList,gridCountX,gridCountY);
+                MoveDirection m=pathFind.FindNextStep(i);
+                if (checkIfEat(new Point (s.head().X,s.head().Y),m)){
+                    s.growHead(m);}
+                else{
+                    s.move(m); }
+
+
+                    repaint();
+
+
+                if (((new Date()).getTime() - c0.getTime())<25) Sleep(25);
+            }
+            //paintComponent(this.getGraphics());
+        }
+    }
+
+    private Point GetNewFoodPoint() {
+        Random random = new Random();
+
+        Point p = new Point (-1,-1);
+        boolean b = true;
+        while (b) {
+            b = false;
+            p = new Point( random.nextInt(gridCountX), random.nextInt(gridCountY));
+
+            for (int i = 0; i < foodList.size(); i++) {
+                if (p.equals(foodList.get(i))) b=true;
+            }
+            for (int i = 0; i < snakeList.size(); i++) {
+                LinkedList<Point> body = snakeList.get(i).getBody();
+                Color color = snakeList.get(i).getColor();
+                for (int j = 0; j < body.size(); j++) {
+                    if (p.equals(body.get(j))) b=true;
+                }
+            }
+
+        }
+        return p;
+    }
+
+    private boolean checkIfEat(Point head,MoveDirection m){
+        boolean bCheck=false;
+        switch (m) {
+                case DOWN:
+                    head.Y++;
+                    break;
+                case UP:
+                    head.Y--;
+                    break;
+                case LEFT:
+                    head.X--;
+                    break;
+                case RIGHT:
+                    head.X++;
+                    break;
+        }
+
+        for (int i=0 ; i<foodList.size();i++) {
+            if (head.equals(foodList.get(i))) {
+                bCheck=true;
+                Point p = GetNewFoodPoint();
+                foodList.get(i).X=p.X;
+                foodList.get(i).Y=p.Y;
+            }
+        }
+        return bCheck;
+    }
+
+
+    public HelloComponent3(int gridX, int gridY, ArrayList<Snake> snakeList, ArrayList<Point> foodList) {
+        gridCountX = gridX;
+        gridCountY = gridY;
+        this.snakeList = snakeList;
+        this.foodList = foodList;
+
+        myThread = new Thread(this);
+        myThread.setDaemon(true);
+        myThread.start();
+    }
+
+    private void drawRectByDesk(Graphics g, int x, int y, Color color){
+        g.setColor(color);
+        //g.drawRect (x*stepX,y*stepY,stepX,stepY);
+        g.fillRect (x*stepX,y*stepY, stepX,stepY);
+    }
+
+    public void paintComponent( Graphics g ) {
+         stepX = this.getSize().width/ gridCountX;
+         stepY = this.getSize().height/ gridCountY;
+
+        //for (int i=0 ; i<gridCountX;i++) g.drawLine(i*stepX,0,i*stepX,this.getSize().height);
+        //for (int i=0 ; i<gridCountY;i++) g.drawLine(0,i*stepY, this.getSize().width,i*stepY);
+
+        for (int i=0 ; i<foodList.size();i++) {
+            drawRectByDesk (g,foodList.get(i).X, foodList.get(i).Y, Color.GREEN);
+        }
+
+        for (int i=0 ; i<snakeList.size();i++) {
+            LinkedList<Point> body = snakeList.get(i).getBody();
+            Color color = snakeList.get(i).getColor();
+            for (int j = 0; j < body.size(); j++) {
+                drawRectByDesk(g, body.get(j).X, body.get(j).Y, color);
+            }
+        }
+    }
+
+}
